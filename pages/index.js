02a496c6e0e4d20cd5a1add29123b487f4d96249
Humberto Mangino <hmangino@gmail.com>
2018-08-18 17:24:15 -0600

    adds date pickeradds next config adds raffleItem

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
-import { Card, Button, Segment } from 'semantic-ui-react';
+import { Card, Button, Segment, Divider } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 import Raffle from '../ethereum/raffle';
 import Layout from '../components/Layout';
-import RafflesList from '../components/RafflesList';
+import RaffleItem from '../components/RaffleItem';
 import { Link } from '../routes';
 class RaffleIndex extends Component {
+	static async getInitialProps(props) {
+		const getRaffles = await factory.methods.getDeployedRaffles().call();
+		return { getRaffles };
+	}
 	render() {
+		let items = null;
+
+		if (this.props.getRaffles) {
+			console.log('this.props.getRaffles ', this.props.getRaffles);
+			items = (
+				<div>
+					{this.props.getRaffles.map((item, index) => {
+						return (
+							<div key={index}>
+								<RaffleItem key={index} rafflename={item} /> <Divider hidden />
+							</div>
+						);
+					})}
+				</div>
+			);
+		}
+
 		return (
 			<Layout>
 				<div>
-					<Segment vertical>
-						<RafflesList />
+					<Segment key={items} vertical>
+						{items}
 					</Segment>
 				</div>
 			</Layout>
